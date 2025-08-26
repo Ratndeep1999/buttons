a6b3f3c1c413e848c05e7a7c2f7b07bf833d7aed
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-26 14:15:31 +0530

    main.dart : buttons add

---
+++ lib/buttons.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Buttons extends StatelessWidget {
+  const Buttons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: (){}, child: Text('ElevatedButton'))
+            ],
+          )
+      ),
+    );
+  }
+}
