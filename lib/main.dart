439248c8a4bdd530994a6dc084c8baddfa0c261b
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-26 14:13:37 +0530

    main.dart : buttons add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'buttons.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,6 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      home: Buttons(),
+    );
   }
 }
