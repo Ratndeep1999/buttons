ad59e0814c11d9422177de8593bf3c02d227f044
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-30 14:51:20 +0530

    buttons.dart : appbar,

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -6,7 +6,7 @@ class Buttons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Text('Buttons')),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
