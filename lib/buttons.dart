01d30b5edd3af9b905302edce1181c31cc97e03c
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-30 15:53:53 +0530

    buttons.dart : appbar, elevated button, text button,

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -25,62 +25,74 @@ class Buttons extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             /// ElevatedButton
-            ElevatedButton(
-              onPressed: () {
-                debugPrint('ElevatedButton');
-                // null : disable state
-              },
-              // If true, button gets focus automatically when screen loads.
-              autofocus: true,
-              // in style we also use ButtonStyle()
-              style: ElevatedButton.styleFrom(
-                //alignment: Alignment.center,
-                backgroundColor: Colors.deepPurple.shade300,
-                foregroundColor: Colors.white,
-                shadowColor: Colors.black,
-                elevation: 15,
-                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                side: BorderSide(width: 3, color: Colors.orangeAccent),
-                // splash enable or disable
-                // splashFactory: NoSplash.splashFactory
-                // this is basically padding vertically and horizontally
-                // visualDensity: VisualDensity(horizontal: 1.5, vertical: 2.5)
-                textStyle: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2,
-                ),
-                // only for web/desktop
-                // enabledMouseCursor: SystemMouseCursors.grab,    // when enabled
-                // disabledMouseCursor: SystemMouseCursors.forbidden, // when disabled
-              ),
-              // call whenever focus state change
-              onFocusChange: (isFocused) {
-                if (isFocused) {
-                  debugPrint("Button is focused!");
-                } else {
-                  debugPrint("Button lost focus!");
-                }
-              },
-              // Called when the mouse pointer enters or exits the button region (desktop/web).
-              onHover: (isHovering) {
-                debugPrint(isHovering ? 'Hovering' : 'Remove Hovering');
-              },
-              onLongPress: (){
-                debugPrint('Long Pressed');
-              },
-              // for keyboard focus control
-              focusNode: FocusNode(),
-              // Controls how content is clipped inside the button (e.g. Clip.antiAlias).
-              clipBehavior: Clip.antiAlias,
-              child: Text('ElevatedButton'),
+            buildElevatedButton(),
+
+            /// Elevated with icon
+            ElevatedButton.icon(
+              onPressed: () {},
+              label: Text('Elevated icon'),
+              icon: Icon(Icons.ads_click),
             ),
+
+            /// TextButton
+            TextButton(onPressed: () {}, child: Text('Text Button')),
           ],
         ),
       ),
+    );
+  }
+
+  ElevatedButton buildElevatedButton() {
+    return ElevatedButton(
+      onPressed: () {
+        debugPrint('ElevatedButton');
+        // null : disable state
+      },
+      // If true, button gets focus automatically when screen loads.
+      autofocus: true,
+      // in style we also use ButtonStyle()
+      style: ElevatedButton.styleFrom(
+        //alignment: Alignment.center,
+        backgroundColor: Colors.deepPurple.shade300,
+        foregroundColor: Colors.white,
+        shadowColor: Colors.black,
+        elevation: 15,
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        side: BorderSide(width: 3, color: Colors.orangeAccent),
+        // splash enable or disable
+        // splashFactory: NoSplash.splashFactory
+        // this is basically padding vertically and horizontally
+        // visualDensity: VisualDensity(horizontal: 1.5, vertical: 2.5)
+        textStyle: TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+        ),
+        // only for web/desktop
+        // enabledMouseCursor: SystemMouseCursors.grab,    // when enabled
+        // disabledMouseCursor: SystemMouseCursors.forbidden, // when disabled
+      ),
+      // call whenever focus state change
+      onFocusChange: (isFocused) {
+        if (isFocused) {
+          debugPrint("Button is focused!");
+        } else {
+          debugPrint("Button lost focus!");
+        }
+      },
+      // Called when the mouse pointer enters or exits the button region (desktop/web).
+      onHover: (isHovering) {
+        debugPrint(isHovering ? 'Hovering' : 'Remove Hovering');
+      },
+      onLongPress: () {
+        debugPrint('Long Pressed');
+      },
+      // for keyboard focus control
+      focusNode: FocusNode(),
+      // Controls how content is clipped inside the button (e.g. Clip.antiAlias).
+      clipBehavior: Clip.antiAlias,
+      child: Text('ElevatedButton'),
     );
   }
 }
