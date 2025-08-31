f4071c9d0366b8c8a4184aa1ccf9de26091e68a6
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-31 12:01:36 +0530

    icon button, toggle icon button add, icon button with style attribute,

--- lib/buttons2.dart
+++ lib/buttons2.dart
@@ -15,7 +15,6 @@ class _Buttons2State extends State<Buttons2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       /// Floating Action Button
       floatingActionButton: FloatingActionButton(onPressed: () {}),
       body: SafeArea(
@@ -23,60 +22,68 @@ class _Buttons2State extends State<Buttons2> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  /// IconButton
+                  IconButton(
+                    onPressed: () {
+                      HapticFeedback.heavyImpact(); // force vibrato on press
+                      debugPrint('Liked..!');
+                    },
+                    onLongPress: () {
+                      debugPrint('Longe Pressed..!');
+                    },
+                    onHover: (isHover) {
+                      debugPrint(isHover ? 'Hovering' : 'Not Hovering');
+                    },
+                    // when pointer appear on button text appear
+                    tooltip: 'Like',
+                    // Color when onPressed is null
+                    disabledColor: Colors.black45,
+                    mouseCursor: SystemMouseCursors.grabbing,
+                    // On web/desktop it remove splash
+                    autofocus: false,
+                    focusNode: FocusNode(),
+                    // This can easily increase or decrease radios of splash
+                    padding: EdgeInsets.all(5.0),
+                    enableFeedback: true,
+                    // Appears when autofocus is true
+                    focusColor: Colors.blue,
+                    // Appear for 1 second when user click
+                    highlightColor: Colors.green,
+                    // Appear on web/desktop when mouse pointer hover on icon
+                    hoverColor: Colors.purple,
+                    iconSize: 40,
+                    icon: Icon(Icons.favorite, color: Colors.red),
+                  ),
 
-              /// IconButton
-              IconButton(
-                onPressed: () {
-                  HapticFeedback.heavyImpact(); // force vibrato on press
-                  debugPrint('Liked..!');
-                },
-                onLongPress: () {
-                  debugPrint('Longe Pressed..!');
-                },
-                onHover: (isHover) {
-                  debugPrint(isHover ? 'Hovering' : 'Not Hovering');
-                },
-                // when pointer appear on button text appear
-                tooltip: 'Like',
-                // Color when onPressed is null
-                disabledColor: Colors.black45,
-                mouseCursor: SystemMouseCursors.grabbing,
-                // On web/desktop it remove splash
-                autofocus: false,
-                focusNode: FocusNode(),
-                // This can easily increase or decrease radios of splash
-                padding: EdgeInsets.all(5.0),
-                enableFeedback: true,
-                // Appears when autofocus is true
-                focusColor: Colors.blue,
-                // Appear for 1 second when user click
-                highlightColor: Colors.green,
-                // Appear on web/desktop when mouse pointer hover on icon
-                hoverColor: Colors.purple,
-                iconSize: 40,
-                icon: Icon(Icons.favorite, color: Colors.red),
-              ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.thumb_up),
+                    style: IconButton.styleFrom(),
+                    iconSize: 40,
+                  ),
 
-              /// ToolTip IconButton
-              IconButton(
-                onPressed: () {
-                  // toggle 
-                  setState(() {
-                    _isLiked = !(_isLiked);
-                    debugPrint(_isLiked ? 'Clicked' : 'UnClicked');
-                  });
-                },
-                icon: Icon(Icons.favorite_border),
-                // If isSelected is true then icon change to selectedIcon
-                isSelected: _isLiked,
-                // This icon appear when isSelected is true
-                selectedIcon: Icon(
-                  Icons.favorite,
-                  color: Colors.red,
-                ),
-                // 'Like' appear when _isLiked is false
-                tooltip: _isLiked ? 'Liked' : 'Like',
-                iconSize: 40,
+                  /// ToolTip IconButton
+                  IconButton(
+                    onPressed: () {
+                      // toggle
+                      setState(() {
+                        _isLiked = !(_isLiked);
+                        debugPrint(_isLiked ? 'Clicked' : 'UnClicked');
+                      });
+                    },
+                    icon: Icon(Icons.favorite_border),
+                    // If isSelected is true then icon change to selectedIcon
+                    isSelected: _isLiked,
+                    // This icon appear when isSelected is true
+                    selectedIcon: Icon(Icons.favorite, color: Colors.red),
+                    // 'Like' appear when _isLiked is false
+                    tooltip: _isLiked ? 'Liked' : 'Like',
+                    iconSize: 40,
+                  ),
+                ],
               ),
 
               /// Raw Material Button
