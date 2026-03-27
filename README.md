91a58bc4f793c07d9cd52a2c22d4a8faa456e3da
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-27 15:31:24 +0530

    Reconnect project

---
+++ README.md
@@ -0,0 +1,24 @@
+# buttons
+
+all flutter buttons
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+# Result images
+![buttons](assets/images/buttons.png)
+![button2](assets/images/buttons2.png)
+![sliding buttons](assets/images/sliding_buttons.png)
+![buttons on web](assets/images/buttons_web.png)
+![button2 on web](assets/images/button2_web.png)
+![sliding buttons on web](assets/images/sliding_buttons_web.png)
---
+++ lib/buttons.dart
@@ -0,0 +1,277 @@
+import 'package:buttons/buttons2.dart';
+import 'package:buttons/sliding_buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:slide_to_act/slide_to_act.dart';
+
+class Buttons extends StatelessWidget {
+  const Buttons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Buttons',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 5,
+          ),
+        ),
+        backgroundColor: Colors.tealAccent.shade100,
+        elevation: 2.0,
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              /// ElevatedButton
+              buildElevatedButton(),
+
+              /// Elevated with icon
+              ElevatedButton.icon(
+                onPressed: () {},
+                label: Text('Elevated icon Button'),
+                icon: Icon(Icons.ads_click),
+                style: OutlinedButton.styleFrom(
+                  enabledMouseCursor: SystemMouseCursors.forbidden, // text),
+                ),
+              ),
+
+              /// TextButton
+              buildTextButton(),
+
+              /// textButton with Icon
+              TextButton.icon(
+                onPressed: () {},
+                label: Text('Text icon Button'),
+                icon: Icon(Icons.ads_click),
+                style: OutlinedButton.styleFrom(
+                  enabledMouseCursor: SystemMouseCursors.allScroll, // text),
+                ),
+              ),
+
+              /// OutlinedButton
+              buildOutlinedButton(context),
+
+              /// Outlined with icon
+              OutlinedButton.icon(
+                onPressed: () {},
+                label: Text('Outlined icon Button'),
+                icon: Icon(Icons.ads_click),
+                style: OutlinedButton.styleFrom(
+                  enabledMouseCursor: SystemMouseCursors.text, // text),
+                ),
+              ),
+
+              /// Swipe Button using Package
+              // slide_to_act: ^2.0.2
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                child: SlideAction(
+                  text: "Go To Sliding Button Screen",
+                  textStyle: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  outerColor: Colors.green,
+                  innerColor: Colors.white,
+                  elevation: 4,
+                  sliderButtonIcon: Icon(
+                    Icons.lock_open,
+                    // Icons.shopping_cart,
+                    color: Colors.green,
+                  ),
+                  onSubmit: () {
+                    // Navigate after swipe
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SlidingButtons()),
+                    );
+                    return null;
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  OutlinedButton buildOutlinedButton(context) {
+    return OutlinedButton(
+      onPressed: () {
+        // Navigation of Second Screen
+        Navigator.of(
+          context,
+        ).push(MaterialPageRoute(builder: (context) => Buttons2()));
+        debugPrint('OutlinedButton');
+      },
+      onLongPress: () {
+        debugPrint('Long Pressed..!');
+      },
+      onHover: (isHover) {
+        debugPrint(isHover ? 'Hover' : 'Hover Remover');
+      },
+      onFocusChange: (isFocused) {
+        debugPrint(isFocused ? 'Focused' : 'Unfocused');
+      },
+      // keyboard focus control
+      focusNode: FocusNode(),
+      // button gets focus automatically on screen load
+      autofocus: true,
+      clipBehavior: Clip.antiAlias,
+      style: OutlinedButton.styleFrom(
+        backgroundColor: Colors.transparent,
+        // This never effect border radios
+        side: BorderSide(color: Colors.orangeAccent, width: 3),
+        // This makes border Rectangular
+        shape: RoundedRectangleBorder(),
+        foregroundColor: Colors.deepOrange,
+        padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+        enableFeedback: true,
+        overlayColor: Colors.black,
+        shadowColor: Colors.grey,
+        elevation: 30.0,
+        enabledMouseCursor: SystemMouseCursors.zoomIn,
+        // text
+        surfaceTintColor: Colors.orange.shade700,
+        // ?
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        // maximumSize: ,
+        // minimumSize: ,
+        // I already specify splash color
+        // splashFactory: ,
+        // It gives Fixed size
+        // fixedSize: Size.square(5.0),
+        textStyle: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+          letterSpacing: 2.0,
+        ),
+      ),
+      child: Text('Go to Second Screen'),
+    );
+  }
+
+  TextButton buildTextButton() {
+    return TextButton(
+      // 1. Callback when tap on button
+      onPressed: () {
+        HapticFeedback.heavyImpact(); // force vibration on press
+        debugPrint('TextButton');
+      },
+      // 2. Callback when longPressed on button
+      onLongPress: () {
+        debugPrint('Long Press..!');
+      },
+      // 3. Callback when mouse enters or leaves the button area (desktop/web).
+      onHover: (isHovering) {
+        debugPrint(isHovering ? "Hovering" : "Not Hovering");
+      },
+      // 4. Callback when the button gains or loses focus.
+      onFocusChange: (isFocused) {
+        debugPrint(isFocused ? "Focused" : "Unfocused");
+      },
+      // For keyboard focus control
+      focusNode: FocusNode(),
+      // If true, button gets focus automatically on screen load.
+      autofocus: true,
+      // Style of text button
+      style: TextButton.styleFrom(
+        // It remover background color
+        backgroundColor: Colors.transparent,
+        // It remover extra surface of button
+        padding: EdgeInsets.all(0.0),
+        // It remover border radius
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0.0)),
+        // It also remove extra surface of button
+        visualDensity: VisualDensity(vertical: 0.0, horizontal: 0.0),
+        // It gives radius (clickable) to the button
+        //fixedSize: Size.fromRadius(50),
+        disabledMouseCursor: SystemMouseCursors.forbidden,
+        // 🚫 cursor
+        enabledMouseCursor: SystemMouseCursors.wait,
+        // like a click sound on web/desktop or haptic vibration on mobile
+        enableFeedback: true,
+        // ❌ no haptic / click sound
+        // controls the color that appears on a button when it is interacted
+        overlayColor: Colors.redAccent,
+        // If you want slower/smoother animations for hover/focus effects on desktop/web.
+        animationDuration: Duration(seconds: 5),
+        textStyle: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.normal,
+          color: Colors.black45,
+        ),
+      ),
+      child: Text('Text Button'),
+    );
+  }
+
+  ElevatedButton buildElevatedButton() {
+    return ElevatedButton(
+      // 1. Callback when tap on button
+      onPressed: () {
+        debugPrint('ElevatedButton');
+        // null : disable state
+      },
+      // 2. Callback when longPressed on button
+      onLongPress: () {
+        debugPrint('Long Pressed');
+      },
+      // 3. Call whenever focus state change
+      onFocusChange: (isFocused) {
+        if (isFocused) {
+          debugPrint("Button is focused!");
+        } else {
+          debugPrint("Button lost focus!");
+        }
+      },
+      // 4. Called when the mouse pointer enters or exits the button region (desktop/web).
+      onHover: (isHovering) {
+        debugPrint(isHovering ? 'Hovering' : 'Remove Hovering');
+      },
+      // If true, button gets focus automatically when screen loads.
+      autofocus: true,
+      // for keyboard focus control
+      focusNode: FocusNode(),
+      // Controls how content is clipped inside the button (e.g. Clip.antiAlias).
+      clipBehavior: Clip.antiAlias,
+      // in style we also use ButtonStyle()
+      style: ElevatedButton.styleFrom(
+        //alignment: Alignment.center,
+        backgroundColor: Colors.deepPurple.shade300,
+        foregroundColor: Colors.white,
+        shadowColor: Colors.black,
+        elevation: 15,
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        side: BorderSide(width: 3, color: Colors.orangeAccent),
+        // splash enable or disable
+        // splashFactory: NoSplash.splashFactory
+        // this is basically padding vertically and horizontally
+        // visualDensity: VisualDensity(horizontal: 1.5, vertical: 2.5)
+        textStyle: TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+        ),
+        // only for web/desktop
+        enabledMouseCursor: SystemMouseCursors.grab,
+        // when enabled
+        disabledMouseCursor: SystemMouseCursors.forbidden, // when disabled
+      ),
+      child: Text('ElevatedButton'),
+    );
+  }
+}
