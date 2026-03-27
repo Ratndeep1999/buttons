7193d5ddf48f5d4cc05ebeab1ec393e39562e01a
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-27 20:43:47 +0530

    main.dart: home: ButtonScreenOne() add.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:buttons/button_screens/button_screen_one.dart';
 import 'package:flutter/material.dart';
-import 'buttons.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +9,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.light(),
       debugShowCheckedModeBanner: false,
-      home: Buttons(),
+      home: ButtonScreenOne(),
     );
   }
 }
