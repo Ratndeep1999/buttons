e82cbb499996645f000e81e26d451173f709712d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-27 16:33:20 +0530

    text_styles.dart: static text stile add.

---
+++ lib/utils/constants/text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class TextStyles {
+  TextStyles._();
+
+  ///
+  static const TextStyle kText30W400 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 5,
+  );
+}
